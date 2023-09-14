87acdfb1e015feaa8849acd8483f51facd92a449
Felipe Dias Gomes <52134440+feliped777@users.noreply.github.com>
2023-09-14 18:02:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const frente = document.querySelector('#frente');
 const fundo = document.querySelector('#fundo');
 const esquerda = document.querySelector('#esquerda');
 const direita = document.querySelector('#direita');
+const metros = document.querySelector('#metros');
+const reshec = document.querySelector('#reshec');
 
 function calcArea() {
   if (frente.value !== '' && fundo.value !== '' && esquerda.value !== '' && direita.value !== '') {
@@ -17,11 +19,23 @@ function calcArea() {
       const result = area1/2 * area2/2;
       let classification = 'Metros Quadrados';
 
-      resultado.innerHTML = `Área: ${result} ${classification}`;
+      resultado.innerHTML = `Área: ${result} ${classification} ou ${result/10000} hectares`;
     } else {
       resultado.innerHTML = 'Preencha todos os campos com valores numéricos';
     }
   } else {
     resultado.innerHTML = 'Preencha todos os campos';
   }
+ 
+}
+function calcHectares () {
+  if (metros.value !== ''){
+    var hectares = parseFloat(metros.value) / 10000;
+    reshec.innerHTML = ` ${hectares} Hectares`;
+
+  } else {
+    reshec.innerHTML = ' Por Favor, Preencha o Campo';
+  }
+
+  
 }
